47273a7ba481e7695a496fd2ea73e1fe574538fd
Igor Shaymukhametov <shaym@yandex.ru>
2023-10-18 01:25:38 +0500

    fix: import problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom/client';
-
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App />);
