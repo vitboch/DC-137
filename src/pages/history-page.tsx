2aab6cf3b87bf7e486c47f3f89ab5547672a24e6
Igor Shaymukhametov <shaym@yandex.ru>
2023-10-25 14:20:45 +0500

    feat: add history header

--- src/pages/history-page.tsx
+++ src/pages/history-page.tsx
@@ -2,6 +2,7 @@ import History from "../components/history/history";
 
 export const HistoryPage = () => {
   return <section className="section">
+    <h2>Search history</h2>
     <History />
   </section>;
 };
