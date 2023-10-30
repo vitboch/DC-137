102b4b15f4f06630fdea633465dde8e24ebc0a88
Igor Shaymukhametov <shaym@yandex.ru>
2023-10-30 12:58:42 +0500

    refactor: app styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,7 @@ import Logo from '../logo';
 import cls from './header.module.css';
 import { useAppSelector } from '../../hooks/redux-hooks';
 import useAuth from '../../hooks/use-auth';
+import { UserProfilePic } from './user-profile-pic';
 
 const Header = () => {
   const { user } = useAppSelector((state) => state.userData);
@@ -14,52 +15,59 @@ const Header = () => {
 
   return (
     <header className={cls.header}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      {user ? (
-        <ul className={cls.nav}>
-          <li>
-            <NavLink to="/characters" className={cls.link}>
-              Characters
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/search" className={cls.link}>
-              Search
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/favorites" className={cls.link}>
-              Favorites
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/history" className={cls.link}>
-              History
-            </NavLink>
-          </li>
-          <li>👤{user?.displayName}</li>
-          <li>
-            <NavLink to="/" className={cls.link} onClick={handleSignOut}>
-              Log out
-            </NavLink>
-          </li>
-        </ul>
-      ) : (
-        <ul className={cls.nav}>
-          <li>
-            <NavLink to="/signin" className={cls.link}>
-              Sign in
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/signup" className={cls.link}>
-              Sign up
-            </NavLink>
-          </li>
-        </ul>
-      )}
+      <div className={cls.header__container}>
+        <Link to="/">
+          <Logo />
+        </Link>
+        {user ? (
+          <>
+            <ul className={cls.nav}>
+              <li>
+                <NavLink to="/characters" className={cls.link}>
+                  Characters
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/search" className={cls.link}>
+                  Search
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/favorites" className={cls.link}>
+                  Favorites
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/history" className={cls.link}>
+                  History
+                </NavLink>
+              </li>
+            </ul>
+            <div className={cls['header__user-section']}>
+              
+              <span className={cls['profile']} ><UserProfilePic /><span className={cls['profile__user-name']}>{ user?.displayName }</span></span>
+              <span>
+                <NavLink to="/" className={cls.link} onClick={handleSignOut}>
+                  Log out
+                </NavLink>
+              </span>
+            </div>
+          </>
+        ) : (
+          <ul className={cls.nav}>
+            <li>
+              <NavLink to="/signin" className={cls.link}>
+                Sign in
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup" className={cls.link}>
+                Sign up
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
     </header>
   );
 };
